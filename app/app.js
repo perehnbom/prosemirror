af4f8e539dec4a689ef5a898ba78ecdbf986c77e
Per Ehnbom <per.ehnbom@unibet.com>
2017-02-27 16:09:38 +0000

    fix

--- app/app.js
+++ app/app.js
@@ -1,32 +1,42 @@
-const {EditorState} = require("prosemirror-state")
-const {MenuBarEditorView} = require("prosemirror-menu")
-const {DOMParser, Schema} = require("prosemirror-model")
-const {schema: baseSchema} = require("prosemirror-schema-basic")
-const {addListNodes} = require("prosemirror-schema-list")
-const {exampleSetup} = require("prosemirror-example-setup")
+
+
+
+const EditorState = require("prosemirror-state").EditorState
+const MenuBarEditorView = require("prosemirror-menu").MenuBarEditorView
+const DOMParser = require("prosemirror-model").DOMParser
+const Schema = require("prosemirror-model").Schema
+
+const addListNodes = require("prosemirror-schema-list").addListNodes
+const exampleSetup = require("prosemirror-example-setup").exampleSetup
+const baseSchema = require("prosemirror-schema-basic").schema
+
+//require("prosemirror-markdown")
+
 
 const schema = new Schema({
   nodes: addListNodes(baseSchema.spec.nodes, "paragraph block*", "block"),
   marks: baseSchema.spec.marks
 })
 
-let content = document.querySelector("#content")
+var content = document.querySelector("#content")
 content.style.display = "none"
 
-let tip = document.querySelector(".demotip")
+var tip = document.querySelector(".demotip")
 
-let doc = DOMParser.fromSchema(schema).parse(content);
+var doc = DOMParser.fromSchema(schema).parse(content);
 
-let view = new MenuBarEditorView(document.querySelector("#editor"), {
+
+var view = new MenuBarEditorView(document.querySelector("#editor"), {
   state: EditorState.create({
     doc: doc,
-    plugins: exampleSetup({schema})
+    plugins: exampleSetup({schema : schema})
   }),
-  onFocus() {
+  onFocus : function() {
     if (tip) {
       tip.innerHTML = "<a href='#demos' style='text-decoration: none; pointer-events: auto; color: inherit'>Find more demos below ↓</a>"
       tip = null
     }
   }
 })
+
 window.view = view.editor
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 var path = require("path");
+var webpack = require("webpack");
+var plugins = [];
+var BUILD = true;
+
+if(BUILD){
+  plugins.push(new webpack.optimize.UglifyJsPlugin({minimize : true}));
+}
+
+
 module.exports = {
+  context: path.resolve(__dirname, './app'),
   entry: {
-    app: ["./app/app.js"]
+    app: "./app.js"
   },
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "./dist"),
     publicPath: "/assets/",
-    filename: "bundle.js"
+    filename: "[name].bundle.js"
   },
+  plugins: plugins,
   devtool: 'inline-source-map'
 };
